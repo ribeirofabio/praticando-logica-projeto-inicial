b96634a49ab93422f8547a34400ae1881d1857b7
Fabio Rodrigues <fabiorr2011@gmail.com>
2024-09-25 18:53:26 -0300

    desafio-confirmar-devolucao

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -4,21 +4,28 @@ function alterarStatus(id) {
     // SELECIONANDO A IMAGEM DO JOGO
     let imagem = gameClicado.querySelector('.dashboard__item__img'); // PROCURAR UM ELEMENTO COM UMA CLASSE ESPECIFICADA (.[classe]) DENTRO DO ID
     // SELECIONANDO O BOTAO DE ALUGAR/DEVOLVER
-    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let botao = gameClicado.querySelector('.dashboard__item__button');        
     // LOGICA PARA ALTERAR O STATUS ATRAVES DA LISTA DE CLASSES DO ELEMENTO
     if (imagem.classList.contains('dashboard__item__img--rented')) {
+        // CONFIRMACAO DE DEVOLUCAO
+        let confirmarDevolucao = prompt('Deseja devolver o jogo (S ou N)?');
+        if(confirmarDevolucao == 's' || confirmarDevolucao == 'S'){
         // REMOVENDO A CLASSE DA LISTA DE CLASSES
         imagem.classList.remove('dashboard__item__img--rented');
         // ALTERAR O TEXTO DO BOTAO
         botao.textContent = 'Alugar';
         // ALTERAR A COR DO BOTAO
         botao.classList.remove('dashboard__item__button--return');
-    } else {
+        }else {
+            return;
+        }    
+    }else {    
+        
         // CASO NAO POSSUA, ADICIONE A CLASSE
         imagem.classList.add('dashboard__item__img--rented');
         // ALTERAR O TEXTO DO BOTAO
         botao.textContent = 'Devolver';
         // ALTERAR A COR DO BOTAO
-        botao.classList.add('dashboard__item__button--return');
+        botao.classList.add('dashboard__item__button--return');        
     }
 }        
