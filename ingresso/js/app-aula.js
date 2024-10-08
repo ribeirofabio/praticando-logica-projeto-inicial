51d54bbe17f36b42f0947c839ca1181d47213b07
Fabio Rodrigues <fabiorr2011@gmail.com>
2024-10-07 23:03:56 -0300

    Obtendo ingresso escolhido

---
+++ ingresso/js/app-aula.js
@@ -0,0 +1,9 @@
+function comprar() {
+    // RECUPERAR O TIPO DE INGRESSO
+    let tipo = document.getElementById('tipo-ingresso');
+    // RECUPERAR A QUANTIDADE INFORMADA
+    let qtd = document.getElementById('qtd');
+
+    alert(tipo.value);
+    alert(qtd.value);
+}
