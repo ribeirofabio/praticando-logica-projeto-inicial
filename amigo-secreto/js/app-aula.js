fca91f052c2373c1025b7b999ede30b8cb0a41fe
Fabio Rodrigues <fabiorr2011@gmail.com>
2024-10-18 17:58:30 -0300

    Embaralhando o array de amigos

--- amigo-secreto/js/app-aula.js
+++ amigo-secreto/js/app-aula.js
@@ -1,8 +1,15 @@
+// VARIAVEIS
+// ARRAY DE AMIGOS
+let amigos = [];
+
+// FUNCOES
 function adicionar() {
     // RECUPERAR NOME DO AMIGO DIGITADO
     let amigo = document.getElementById('nome-amigo');
     // RECUPERAR A LISTA DE AMIGOS
     let lista = document.getElementById('lista-amigos');
+    // ADICIONAR AMIGOS NO ARRAY DE AMIGOS
+    amigos.push(amigo.value);
     // CONDICOES
     // SE A LISTA ESTIVER VAZIA
     if(lista.textContent == '') {
@@ -14,4 +21,23 @@ function adicionar() {
     }
     // LIMPA O CAMPO NOME DO AMIGO
     amigo.value = '';
+}
+
+function sortear() {
+    // EMBARALHAR LISTA DE AMIGOS
+    embaralha(amigos);
+    
+}
+
+// https://cangaceirojavascript.com.br/como-embaralhar-arrays-algoritmo-fisher-yates/
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
