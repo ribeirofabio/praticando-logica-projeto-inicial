352fbb498821bee7490dcf07e9f28de5d51f8c11
Fabio Rodrigues <fabiorr2011@gmail.com>
2024-10-18 18:16:20 -0300

    Realizando o sorteio

--- amigo-secreto/js/app-aula.js
+++ amigo-secreto/js/app-aula.js
@@ -24,9 +24,21 @@ function adicionar() {
 }
 
 function sortear() {
-    // EMBARALHAR LISTA DE AMIGOS
+    // EMBARALHA LISTA DE AMIGOS
     embaralha(amigos);
-    
+    // RECUPERA LISTA DE SORTEIO
+    let sorteio = document.getElementById('lista-sorteio');
+    // ATUALIZA TEXTO DO CAMPO DA LISTA DE SORTEIO
+    for(let i = 0; i < amigos.length; i++){
+        // SE I FOR IGUAL AO ULTIMO ELEMENTO DA LISTA
+        if(i == amigos.length - 1){
+            // O ULTIMO NOME DA LISTA TIRA O PRIMEIRO
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' -- > ' + amigos[0];
+        } else {
+            // O PRIMEIRO TIRA O SEGUNDO E ASSIM POR DIANTE ATE O FINAL DA LISTA
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' -- > ' + amigos[i + 1] + '<br>';
+        }        
+    }
 }
 
 // https://cangaceirojavascript.com.br/como-embaralhar-arrays-algoritmo-fisher-yates/
