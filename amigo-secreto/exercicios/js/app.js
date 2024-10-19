9d9cc0f00ba2895c26341199be3a7f55632763c8
Fabio Rodrigues <fabiorr2011@gmail.com>
2024-10-18 21:04:49 -0300

    Loops com for

---
+++ amigo-secreto/exercicios/js/app.js
@@ -0,0 +1,8 @@
+multiplosDeCinco();
+function multiplosDeCinco() {
+    for (let numero = 1; numero <= 100; numero++) {
+        if (numero % 5 == 0) {
+            console.log(numero);
+        }
+    }    
+}
